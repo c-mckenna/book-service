e9370fc123ce5f2570001185dcf0fb6f2229621d
Callum McKenna <callum.mckenna@dva.gov.au>
2017-05-04 20:12:06 +1000

    Added @CrossOrigin to allow cross-domain requests (this config isn't  restrictive enough for production use)

--- src/main/java/au/id/callum/bookservice/repositories/BookRepository.java
+++ src/main/java/au/id/callum/bookservice/repositories/BookRepository.java
@@ -3,7 +3,9 @@ package au.id.callum.bookservice.repositories;
 import au.id.callum.bookservice.domain.Book;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 }
--- src/main/java/au/id/callum/bookservice/repositories/PersonRepository.java
+++ src/main/java/au/id/callum/bookservice/repositories/PersonRepository.java
@@ -4,7 +4,9 @@ import au.id.callum.bookservice.domain.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 }
